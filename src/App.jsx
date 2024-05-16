5c869d179ded2ccde4b943ffeb85a82386b1a7bc
ChNaressiff <charlesnaressi14@gmail.com>
2024-05-15 21:57:00 -0300

    feat: create function React

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css"
+import api from "./assets/images/svg/api-interface-svgrepo-com.svg"
+import availability from "./assets/images/svg/availability-svgrepo-com.svg"
+import cloud from "./assets/images/svg/cloud-acceleration-svgrepo-com.svg"
+import flexible from "./assets/images/svg/flexible-access-svgrepo-com.svg"
+import intelligent from "./assets/images/svg/intelligent-positioning-svgrepo-com.svg"
+import control from "./assets/images/svg/interface-control-svgrepo-com.svg"
+
+function Banner(props){
+  return (
+    <div className="banner">
+      <img src={props.image} alt="" />
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <body>
+      <div className="card">
+          <h1>React Course</h1>
+          <p>Front-End Javascript</p>
+      </div>
+      <Banner title="Título" description="Descrição" image={api}/>
+      <Banner title="Título" description="Descrição" image={availability}/>
+      <Banner title="Título" description="Descrição" image={cloud}/>
+      <Banner title="Título" description="Descrição" image={flexible}/>
+      <Banner title="Título" description="Descrição" image={intelligent}/>
+      <Banner title="Título" description="Descrição" image={control}/>
+
+    </body>
+  );
+}
+
+export default App;
---
+++ src/scripts/nomenclature.js
@@ -0,0 +1,17 @@
+ // nomenclatura de variável
+
+ const userName = "Charles Naressi"
+
+ const user_name = "Charles Naressi"
+
+ // nomenclatura de funções
+
+ function SomaNumeros(a, b) {
+    return a + b
+ }
+
+ const primeiroNumero = 10
+
+ const segundoNumero = 20
+
+ console.log(SomaNumeros(primeiroNumero, segundoNumero))
