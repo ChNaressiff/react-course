09b81a515b655d60afb6d579d7a130e9a66aa6e7
ChNaressiff <charlesnaressi14@gmail.com>
2024-05-15 22:10:30 -0300

    fix: correction function React

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import "./App.css"
+
+import Banner from "./Components/Banner"
+
 import api from "./assets/images/svg/api-interface-svgrepo-com.svg"
 import availability from "./assets/images/svg/availability-svgrepo-com.svg"
 import cloud from "./assets/images/svg/cloud-acceleration-svgrepo-com.svg"
 import flexible from "./assets/images/svg/flexible-access-svgrepo-com.svg"
 import intelligent from "./assets/images/svg/intelligent-positioning-svgrepo-com.svg"
 import control from "./assets/images/svg/interface-control-svgrepo-com.svg"
-
-function Banner(props){
-  return (
-    <div className="banner">
-      <img src={props.image} alt="" />
-      <div>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-      </div>
-    </div>
-  )
-}
 
 function App() {
   return (
